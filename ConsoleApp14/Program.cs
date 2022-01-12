8f8bd21145409a177dbf0673b0d49890710ecb20
ACER <ACER@DESKTOP-1BR1UTS>
2022-01-12 13:43:53 +0530

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new EmployeeContext())
+            {
+                List<EmployeeDetails> employeeDetails = context.EmployeeDetails.ToList();
+                foreach (var employeeDetail in employeeDetails )
+                {
+                    Console.WriteLine(employeeDetail.Address);
+                }
+            }
+        }
+    }
+}
